6ef1436fbd5eefd1a0ad923d0c22301cd21a0a09
Robert Dodier <robert_dodier@users.sourceforge.net>
2015-02-12 14:51:45 -0800

    Fix up day by day plots for count data.

--- exploratory.r
+++ exploratory.r
@@ -15,11 +15,20 @@ train.timestamp <- strptime (train.1, "%Y-%m-%d %H:%M:%S")
 plot (train.timestamp, train$count, type='l')
 
 plot (c(0, 25), c(0, 1000), type='n')
-for (i in 181:250) { lines (1:24, train$count[(i*24):((i+1)*24 - 1)]) }
+for (i in 1:30) {
+  filter <- train.timestamp$year==111 & train.timestamp$mon==6 & train.timestamp$mday==i
+  lines (train.timestamp$hour[filter], train$count[filter])
+}
 
 plot (c(0, 25), c(0, 1000), type='n')
-for (i in 181:250) { lines (1:24, train$registered[(i*24):((i+1)*24 - 1)]) }
+for (i in 1:30) {
+  filter <- train.timestamp$year==111 & train.timestamp$mon==6 & train.timestamp$mday==i
+  lines (train.timestamp$hour[filter], train$registered[filter])
+}
 
 plot (c(0, 25), c(0, 1000), type='n')
-for (i in 181:250) { lines (1:24, train$casual[(i*24):((i+1)*24 - 1)]) }
+for (i in 1:30) {
+  filter <- train.timestamp$year==111 & train.timestamp$mon==6 & train.timestamp$mday==i
+  lines (train.timestamp$hour[filter], train$casual[filter])
+}
 
