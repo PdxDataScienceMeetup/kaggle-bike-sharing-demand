56a5106e5343a81854f1e6dd1415aeea844435d9
Robert Dodier <robert_dodier@users.sourceforge.net>
2015-02-12 14:34:16 -0800

    Modify the plots for count, registered, and casual.

--- exploratory.r
+++ exploratory.r
@@ -14,15 +14,12 @@ train.1 <- as.vector (train[, 1])
 train.timestamp <- strptime (train.1, "%Y-%m-%d %H:%M:%S")
 plot (train.timestamp, train$count, type='l')
 
-i<-150
-plot (1:24, train$count[(i*24):((i+1)*24-1)], type='l')
-for (i in 151:225) { lines (1:24, train$count[(i*24):((i+1)*24 - 1)]) }
+plot (c(0, 25), c(0, 1000), type='n')
+for (i in 181:250) { lines (1:24, train$count[(i*24):((i+1)*24 - 1)]) }
 
-i<-150
-plot (1:24, train$registered[(i*24):((i+1)*24-1)], type='l')
-for (i in 151:225) { lines (1:24, train$registered[(i*24):((i+1)*24 - 1)]) }
+plot (c(0, 25), c(0, 1000), type='n')
+for (i in 181:250) { lines (1:24, train$registered[(i*24):((i+1)*24 - 1)]) }
 
-i<-150
-plot (1:24, train$casual[(i*24):((i+1)*24-1)], type='l')
-for (i in 151:225) { lines (1:24, train$casual[(i*24):((i+1)*24 - 1)]) }
+plot (c(0, 25), c(0, 1000), type='n')
+for (i in 181:250) { lines (1:24, train$casual[(i*24):((i+1)*24 - 1)]) }
 
